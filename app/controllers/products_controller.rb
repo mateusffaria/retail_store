b73e701e6d53fc7a32ab6c9713f0cea2035be928
Mateus Faria <mateusjose555@gmail.com>
2020-01-04 13:26:11 -0300

    Added the pages of product#new and product#edit, with validations for create and update an product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,47 @@
 class ProductsController < ApplicationController
 
+  before_action :set_product, only:[:edit, :update, :destroy]
+
   def index
     @products = Product.all
+    @best_offer = Product.order(price: :desc).limit 1
+
   end
 
   def new
     @product = Product.new
-    
   end
 
+  def create
+    @product = Product.new product_params
+    if(@product.save)
+      flash[:notice] = "Product successfully created"
+      redirect_to new_product_path
+    else
+      render :new
+    end
+  end
 
+  def edit
+
+  end
+
+  def update
+    if @product.update product_params
+      flash[:notice] = "Product successfully updated"
+      redirect_to root_url
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :amount, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  validates :name, :description, :amount, :price, presence: true
 end
